75bb13411b7c4b58feca8b70e0314adfff1f2118
ashlesha <ashlesha.ashture@anka.co.in>
2022-09-12 18:11:13 +0530

    Assignment searchSort file pushed by ashlesha on 12 sept 2022

--- searchSort.js
+++ searchSort.js
@@ -1,28 +1,20 @@
-let searchNo = [5,-2,6,0,-1];
-let val = 6;
+let searchArray = [5,-2,6,0,-1];
+let val = -2;
 function sorted(){
-for(i=0;i<searchNo.length;i++){
-    for(j=0;j<i;j++){
-        if(searchNo[i]>searchNo[j]){
-            let x = searchNo[i];
-            searchNo[i] = searchNo[j];
-            searchNo[j] = x;
+for(let i=0;i<searchArray.length;i++){
+    for(let j=0;j<i;j++){
+        if(searchArray[i]>searchArray[j]){
+            let x = searchArray[i];
+            searchArray[i] = searchArray[j];
+            searchArray[j] = x;
         }
     }
 }
-    console.log(searchNo +' is sorted searchNo');
-    for(i=0;i<searchNo.length;i++){
-     if(searchNo[i] == val){
+    console.log(searchArray +' is sorted searchArray');
+    for(let i=0;i<searchArray.length;i++){
+     if(searchArray[i] == val){
         console.log(val+ " is on " + [i+1] +' position');
        }
     }   
 }
 sorted();
-
-
-//for(var i=0;i<searchNo.length;i++){}
-/*for(k=0;k<searchNo.length;k++)
-    for(j=0;j<i;j++)
-        if(searchNo[k] == 5){
-        console.log('5 is on ' + searchNo[k] +' position');
-    }*/
