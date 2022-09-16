f0af9c048b4dd5c183aa12065684f95dfa6f0229
rutuja <rutuja.chandgude@anka.co.in>
2022-09-16 18:59:21 +0530

    Assignment of Registration and Login page  pushed by rutuja on 16-09-2022

---
+++ 16_sep22/Registration/registration.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('#form');
+const firstName= document.querySelector('#fname');
+const lasttName= document.querySelector('#lname');
+const email = document.querySelector('#emailid');
+const mobileNo=document.querySelector('#mobileno');
+const password=document.querySelector('#password');
+const cpassword=document.querySelector('#cpassword');
+
