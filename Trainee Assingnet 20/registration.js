ef91efb201abb784aff0aef334d384e93e0cd91a
rushikesh <rushikesh.gawade@anka.co.in>
2022-09-20 18:59:53 +0530

    traning Assingnet 20 push by rushikesh 20/09/22

---
+++ Trainee Assingnet 20/registration.js
@@ -0,0 +1,38 @@
+       function validation(){
+        let fname = document.getElementById('user').value;
+
+        if(fname == ""){
+            document.getElementById('username').innerHTML= "please fill the user name filed";
+            return false;
+        }
+
+        
+
+       }
+       
+     
+     
+
+   
+        
+
+
+
+       
+       
+
+        
+
+
+    
+
+ 
+
+
+    
+    
+
+
+
+
+
