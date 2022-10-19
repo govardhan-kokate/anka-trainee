79202bfef0ef0ea97ca2bfffac0c619ed60f5d2e
Ashlesha <ashlesha.ashture@anka.co.in>
2022-10-19 18:53:48 +0530

    Task App file filtering,sorting,pagination and file uploads changes pushed by Ashlesha on 19 oct 2022

--- node.js/task-manager/src/routers/task.js
+++ node.js/task-manager/src/routers/task.js
@@ -18,8 +18,27 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+    const sort = {}
+
+    if(req.query.completed){
+        match.completed = req.query.completed === 'true'
+    }
+    
+    if(req.query.sortBy){
+        const parts = req.query.sortBy.split(":")
+        sort[parts[0]] = parts[1] === "desc" ? -1 : 1
+    }
     try {
-        await req.user.populate('tasks').execPopulate()
+        await req.user.populate({
+            path : 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send()
@@ -68,7 +87,7 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 
 router.delete('/tasks/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
+        const task = await Task.findOneAndDelete({ _id: req.params.id})
 
         if (!task) {
             res.status(404).send()
