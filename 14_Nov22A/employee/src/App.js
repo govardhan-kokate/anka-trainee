291994e3a835bdfc163ca6c8ef15fb36167ba408
rutuja <rutuja.chandgude@anka.co.in>
2022-11-15 09:43:47 +0530

    ,employee using Material ui  added by Rutuja

---
+++ 14_Nov22A/employee/src/App.js
@@ -0,0 +1,12 @@
+import CreateEmployee from '../src/employees/CreateEmployee'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <CreateEmployee />
+    </div>
+  );
+}
+
+export default App;
